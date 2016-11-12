2c969dcf8f49b206f2e9568dba46eaaa14ccebfd
Marek Melichar <koprivackej@gmail.com>
2016-11-12 22:35:01 +0100

    added toys

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'react-mounter';
 
-import App from './App';
+import App from '../App';
 import MainLayout from './layouts/MainLayout';
 
 FlowRouter.route('/', {
